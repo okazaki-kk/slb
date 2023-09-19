30168d6766d574c4328a971f4ac07e0f49787635
okazaki-kk <kohei-okazaki@c-fo.com>
2023-09-19 09:20:43 +0900

    :+1: add healthcheck

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
+	"net"
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -28,6 +31,30 @@ func (s *Server) SetAlive(alive bool) {
 	s.mux.Lock()
 	defer s.mux.Unlock()
 	s.Alive = alive
+}
+
+func isBackendAlive(u *url.URL) bool {
+	timeout := 2 * time.Second
+	conn, err := net.DialTimeout("tcp", u.Host, timeout)
+	if err != nil {
+		return false
+	}
+
+	defer conn.Close()
+	return true
+}
+
+// HealthCheck pings the backends and update the status
+func (s *ServerPool) HealthCheck() {
+	for _, b := range s.Servers {
+		status := "up"
+		alive := isBackendAlive(&b.URL)
+		b.SetAlive(alive)
+		if !alive {
+			status = "down"
+		}
+		log.Printf("%s [%s]\n", &b.URL, status)
+	}
 }
 
 func main() {
