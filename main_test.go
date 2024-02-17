8fd79da28d82eb408514f53b9a333092ac928c86
okazaki-kk <kohei-okazaki@c-fo.com>
2024-02-17 20:06:25 +0900

    add test

--- main_test.go
+++ main_test.go
@@ -28,3 +28,39 @@ func TestSetServerStatus(t *testing.T) {
 
 	assert.Equal(t, false, serverPool.servers[0].Alive)
 }
+
+func TestGetNextAlive(t *testing.T) {
+	servers := []*Server{
+		{
+			URL:   &url.URL{Host: "localhost:3000"},
+			Alive: true,
+		},
+		{
+			URL:   &url.URL{Host: "localhost:3001"},
+			Alive: false,
+		},
+		{
+			URL:   &url.URL{Host: "localhost:3002"},
+			Alive: false,
+		},
+		{
+			URL:   &url.URL{Host: "localhost:3003"},
+			Alive: true,
+		},
+	}
+	serverPool := &ServerPool{
+		servers: servers,
+		current: 0,
+	}
+
+	s1 := serverPool.GetNextAlive()
+	assert.Equal(t, "localhost:3003", s1.URL.Host)
+
+	s2 := serverPool.GetNextAlive()
+	assert.Equal(t, "localhost:3000", s2.URL.Host)
+
+	u := url.URL{Host: "localhost:3002"}
+	serverPool.SetServerStatus(&u, true)
+	s3 := serverPool.GetNextAlive()
+	assert.Equal(t, "localhost:3002", s3.URL.Host)
+}
