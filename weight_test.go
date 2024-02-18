b9702aad128d3864db584eca04d3689f2274ce3c
okazaki-kk <kohei-okazaki@c-fo.com>
2024-02-18 22:14:10 +0900

    refactor test

--- weight_test.go
+++ weight_test.go
@@ -11,12 +11,12 @@ func TestSet(t *testing.T) {
 	cases := []struct {
 		name   string
 		input  *WeightedServerPool
-		output *WeightedServerPool
+		outputWeight float64
 	}{
 		{
 			name:   "Simple test 1",
 			input:  New([]*WeightedServer{}),
-			output: &WeightedServerPool{servers: []*WeightedServer{}, Weight: 0},
+			outputWeight: 0.0,
 		},
 		{
 			name: "Simple test 2",
@@ -27,7 +27,23 @@ func TestSet(t *testing.T) {
 					Weight: 0.3,
 				},
 			}),
-			output: &WeightedServerPool{servers: []*WeightedServer{}, Weight: 0.3},
+			outputWeight: 0.3,
+		},
+		{
+			name: "Simple test 3",
+			input: New([]*WeightedServer{
+				{
+					URL:    &url.URL{Host: "localhost:3000"},
+					Alive:  true,
+					Weight: 0.3,
+				},
+				{
+					URL:    &url.URL{Host: "localhost:3001"},
+					Alive:  true,
+					Weight: 1.2,
+				},
+			}),
+			outputWeight: 1.5,
 		},
 	}
 
@@ -35,7 +51,7 @@ func TestSet(t *testing.T) {
 		tt := tt
 		wsp := &WeightedServerPool{}
 		wsp.Set(tt.input.servers)
-		assert.Equal(t, tt.output.Weight, wsp.Weight, tt.name)
+		assert.Equal(t, tt.outputWeight, wsp.Weight, tt.name)
 		assert.Equal(t, len(tt.input.servers), len(wsp.servers), tt.name)
 	}
 }
