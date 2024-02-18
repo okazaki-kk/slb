2233c18158ff38b29b05c61a031a50a82cad4925
okazaki-kk <kohei-okazaki@c-fo.com>
2024-02-18 22:06:44 +0900

    add weighted round robin algorithm

---
+++ weight.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"math/rand"
+	"net/http/httputil"
+	"net/url"
+	"sort"
+	"sync"
+)
+
+const (
+	DefaultWeight = 1.0
+	BTreeBorder   = 10
+)
+
+type WeightedServer struct {
+	URL          *url.URL
+	Alive        bool
+	mu           sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+	Weight       float64
+}
+
+type WeightedServerPool struct {
+	servers []*WeightedServer
+	current int
+	Weight  float64
+	Rand    *rand.Rand
+}
+
+type Option struct {
+	DefaultWeight int
+	BTreeBorder   int
+}
+
+func (s *WeightedServerPool) Len() int {
+	return len(s.servers)
+}
+
+func (s *WeightedServerPool) Swap(i, j int) {
+	s.servers[i], s.servers[j] = s.servers[j], s.servers[i]
+}
+
+func (s *WeightedServerPool) Less(i, j int) bool {
+	return s.servers[i].Weight < s.servers[j].Weight
+}
+
+func (s *WeightedServerPool) Set(list []*WeightedServer) {
+	sortedPool := WeightedServerPool{
+		servers: list,
+	}
+	sort.Sort(&sortedPool)
+}
+
+func New(list []*WeightedServer) *WeightedServerPool {
+	wsp := &WeightedServerPool{
+		servers: list,
+		Weight:  0,
+		Rand:    rand.New(rand.NewSource(0)),
+	}
+
+	return wsp
+}
---
+++ weight_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSet(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  *WeightedServerPool
+		output *WeightedServerPool
+	}{
+		{
+			name:   "Simple test 1",
+			input:  New([]*WeightedServer{}),
+			output: &WeightedServerPool{servers: []*WeightedServer{}, Weight: 0},
+		},
+		{
+			name: "Simple test 2",
+			input: New([]*WeightedServer{
+				{
+					URL:    &url.URL{Host: "localhost:3000"},
+					Alive:  true,
+					Weight: 0.3,
+				},
+			}),
+			output: &WeightedServerPool{servers: []*WeightedServer{}, Weight: 0},
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		wsp := &WeightedServerPool{}
+		wsp.Set(tt.input.servers)
+		assert.Equal(t, tt.output.Weight, wsp.Weight)
+	}
+}
